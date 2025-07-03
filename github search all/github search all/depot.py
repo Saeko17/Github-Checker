569373f55ae1574adaf542ccb8d0659c88c1eb2a
Saeko17 <emailpourgithub@gmail.com>
2025-07-03 03:48:00 +0200

    Add files via upload

---
+++ github search all/github search all/depot.py
@@ -0,0 +1,15 @@
+import requests
+
+
+def list_repositories(username):
+    url = f"https://api.github.com/users/{username}/repos"
+    response = requests.get(url)
+    if response.status_code == 200:
+        repos = response.json()
+        for repo in repos:
+            print(f"- {repo['name']} (⭐ {repo['stargazers_count']}) - Langage : {repo['language']}")
+    else:
+        print("Erreur lors de la récupération des dépôts.")
+
+# Exemple d'utilisation
+list_repositories("")
